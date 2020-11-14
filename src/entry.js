d849cac26d6ce9f6e998e2ad0fa299ef9bcce8de
你好2007 <2501482523@qq.com>
2020-11-14 17:52:58 +0800

    添加基础js包统计

--- src/entry.js
+++ src/entry.js
@@ -6,6 +6,7 @@ import App from './App.iCrush';
 // 引入样式
 import "@yelloxing/normalize.css";
 import './styles/common.css';
+import './styles/npm.css';
 
 // 引入路由
 import routers from './routers/index.js';
@@ -13,10 +14,10 @@ iCrush.use(routers);
 
 window.icrush = new iCrush({
 
-  // 挂载点
-  el: document.getElementById('root'),
+    // 挂载点
+    el: document.getElementById('root'),
 
-  // 启动组件
-  render: createElement => createElement(App)
+    // 启动组件
+    render: createElement => createElement(App)
 
 });
