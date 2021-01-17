3e38cd92c4dc209b734b45a2f9304ac6a4e00efd
你好2007 <2501482523@qq.com>
2021-01-17 13:48:25 +0800

    请求统计优化

--- src/entry.js
+++ src/entry.js
@@ -13,6 +13,8 @@ import './styles/doc-view.css';
 import routers from './routers/index.js';
 QuickPaper.use(routers);
 
+window.hai2007_sweethome_counterUrl = "https://s05.flagcounter.com/count2/bdd0/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_12/viewers_3/labels_1/pageviews_1/flags_0/percent_0/";
+
 window.quickPaper = new QuickPaper({
 
     // 挂载点
