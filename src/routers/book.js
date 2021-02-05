30b21d2fad2a15609caafc18fadc0382a4064882
你好2007 <2501482523@qq.com>
2021-02-05 16:26:30 +0800

    升级框架以支持IE浏览器

--- src/routers/book.js
+++ src/routers/book.js
@@ -1,5 +1,15 @@
 export default {
     webpack: {
-        content: () => import('../pages/books/webpack/index.paper')
+        content: () => import('../pages/books/webpack/index.paper'),
+        before: {
+            content: () => import('../pages/books/webpack/before.paper'),
+        },
+        loader: {
+            content: () => import('../pages/books/webpack/loader.paper'),
+        },
+        plug: {
+            content: () => import('../pages/books/webpack/plug.paper'),
+        },
+        _default_: "before"
     }
 };
