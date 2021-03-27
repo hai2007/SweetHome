6744efca814a72ea08d70d432286a8b509305b5c
你好2007 <2501482523@qq.com>
2021-03-27 12:32:17 +0800

    html入门教程写完

--- src/pages/editor/examples/lazy-load.js
+++ src/pages/editor/examples/lazy-load.js
@@ -3,5 +3,6 @@ export default {
     // 入门小教程
     "learning_html-style": () => import('./learning/html-style.html'),
     "learning_html-script": () => import('./learning/html-script.html'),
+    "learning_html-canvas": () => import('./learning/html-canvas.html'),
 
 };
