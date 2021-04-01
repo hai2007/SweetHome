517589c7b1d7ef10d4099143dbd9d52b4ce442c6
你好2007 <2501482523@qq.com>
2021-04-01 14:16:23 +0800

    添加css百分比用例

--- src/pages/editor/examples/lazy-load.js
+++ src/pages/editor/examples/lazy-load.js
@@ -5,4 +5,13 @@ export default {
     "learning_html-script": () => import('./learning/html-script.html'),
     "learning_html-canvas": () => import('./learning/html-canvas.html'),
 
+    // style.css
+    "style_percent-padding": () => import('./style.css/percent-padding.html'),
+    "style_percent-padding#absolute": () => import('./style.css/percent-padding#absolute.html'),
+    "style_percent-padding#layout": () => import('./style.css/percent-padding#layout.html'),
+    "style_percent-height": () => import('./style.css/percent-height.html'),
+    "style_percent-height#value": () => import('./style.css/percent-height#value.html'),
+    "style_percent-height#absolute": () => import('./style.css/percent-height#absolute.html'),
+    "style_percent-height#value+absolute": () => import('./style.css/percent-height#value+absolute.html'),
+
 };
