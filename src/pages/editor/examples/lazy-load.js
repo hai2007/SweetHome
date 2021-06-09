d8485d638c51ffa338221414165f52577406d106
你好2007 <2501482523@qq.com>
2021-06-09 16:04:39 +0800

    添加准备整理的教程

--- src/pages/editor/examples/lazy-load.js
+++ src/pages/editor/examples/lazy-load.js
@@ -1,6 +1,6 @@
 export default {
 
-    // 入门小教程
+    // 小课堂
     "learning_html-style": () => import('./learning/html-style.html'),
     "learning_html-script": () => import('./learning/html-script.html'),
     "learning_html-canvas": () => import('./learning/html-canvas.html'),
