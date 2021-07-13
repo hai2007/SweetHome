e1e5a7e64ad0f0070cf1aa4b8065f2cf91368249
你好2007 <2501482523@qq.com>
2021-07-13 17:44:46 +0800

    添加全局按钮（预留）

--- dist/sweethome@v1.19.8-bundle0.js
+++ dist/sweethome@v1.19.8-bundle0.js
@@ -1,4 +1,4 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{53:function(e,t,r){(function(e){
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{54:function(e,t,r){(function(e){
 /*!
  * clunch.js - 🎨 The Progressive JavaScript Interactive Picture Framework.
  * git+https://github.com/hai2007/clunch.git
